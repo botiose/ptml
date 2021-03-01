0b235c865611a6d5887e77ef7e32740106dd8d75
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2021-03-01 14:39:35 +0100

    ml02(fix): exe08

--- part/otiose/ml02/exe08.py
+++ part/otiose/ml02/exe08.py
@@ -6,7 +6,6 @@ data1 = pd.read_csv("data1.csv")[["age", "fare", "survived", "sex"]].dropna()
 
 d = {"male": 1, 'female': 0}
 data1.sex = data1.sex.map(d)
-data1.sex = data1.sex.apply(lambda x: x + np.random.uniform(0, 0.1))
 
 colors = ["red" if i == 1 else "blue" for i in data1.survived]
 
